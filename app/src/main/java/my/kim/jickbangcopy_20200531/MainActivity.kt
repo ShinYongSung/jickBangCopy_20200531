01f543010776d307573d4c2a883d22d35f604524
ShinYongSUng <newxm@naver.com>
2020-05-31 14:55:55 +0900

    manifest 시각정보 수정

--- app/src/main/java/my/kim/jickbangcopy_20200531/MainActivity.kt
+++ app/src/main/java/my/kim/jickbangcopy_20200531/MainActivity.kt
@@ -2,6 +2,7 @@ package my.kim.jickbangcopy_20200531
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import my.kim.jickbangcopy_20200531.adapters.RoomAdapter
 import my.kim.jickbangcopy_20200531.datas.Room
@@ -20,6 +21,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        roomListView.setOnItemLongClickListener { parent, view, position, id ->  }
+
+        val alert = AlertDialog.Builder(mContext)
+        alert.setTitle("테스트")
+        alert.setMessage("테스트용 얼럿")
+        alert.setPositiveButton("확인", null)
+        alert.show()
 
 
     }
