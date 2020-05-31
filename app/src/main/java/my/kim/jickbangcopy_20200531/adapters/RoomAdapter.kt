7401cdffd8ea8a3df4ce4e727bae04d350838e37
ShinYongSUng <newxm@naver.com>
2020-05-31 12:24:24 +0900

    RoomAdapter뼈대작업 종료

--- app/src/main/java/my/kim/jickbangcopy_20200531/adapters/RoomAdapter.kt
+++ app/src/main/java/my/kim/jickbangcopy_20200531/adapters/RoomAdapter.kt
@@ -2,8 +2,32 @@ package my.kim.jickbangcopy_20200531.adapters
 
 import android.content.Context
 import android.content.LocusId
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import my.kim.jickbangcopy_20200531.R
 import my.kim.jickbangcopy_20200531.datas.Room
 
 class RoomAdapter(context:Context, resId: Int, list: List<Room>) : ArrayAdapter<Room>(context, resId, list) {
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+
+            tempRow = inf.inflate(R.layout.room_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
 }
