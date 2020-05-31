669de0c4d58dd256a795924ee3466aee676d7371
ShinYongSUng <newxm@naver.com>
2020-05-31 12:12:08 +0900

    메인액티비티 - BaseActivity 상속으로 변경

--- app/src/main/java/my/kim/jickbangcopy_20200531/MainActivity.kt
+++ app/src/main/java/my/kim/jickbangcopy_20200531/MainActivity.kt
@@ -3,12 +3,24 @@ package my.kim.jickbangcopy_20200531
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setvalues()
+    }
 
+    override fun setupEvents() {
 
     }
+
+    override fun setvalues() {
+
+    }
+
+
+
+
+
 }
