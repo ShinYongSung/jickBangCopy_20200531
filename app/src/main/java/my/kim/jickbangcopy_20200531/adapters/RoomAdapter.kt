e16a1ac1a21444d08bec0308aa2aeb334de32012
ShinYongSUng <newxm@naver.com>
2020-05-31 12:02:11 +0900

    어댑터 생성자 / 상속 처리

---
+++ app/src/main/java/my/kim/jickbangcopy_20200531/adapters/RoomAdapter.kt
@@ -0,0 +1,9 @@
+package my.kim.jickbangcopy_20200531.adapters
+
+import android.content.Context
+import android.content.LocusId
+import android.widget.ArrayAdapter
+import my.kim.jickbangcopy_20200531.datas.Room
+
+class RoomAdapter(context:Context, resId: Int, list: List<Room>) : ArrayAdapter<Room>(context, resId, list) {
+}
